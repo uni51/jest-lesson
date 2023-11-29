00237dbc1c1227a815fb13cd67e9922bae2cbd4d
uni51 <unieye51@gmail.com>
2023-11-29 09:06:23 +0900

    14. パラメタライズドテスト

--- src/section2/sum.test.ts
+++ src/section2/sum.test.ts
@@ -1,5 +1,17 @@
 import { sum } from "./sum";
 
-it("1と2を足すとを足すと3になる", () => {
-  expect(sum(1, 2)).toBe(3);
+// it("1と2を足すとを足すと3になる", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+// it("1と-2を足すとを足すと-1になる", () => {
+//   expect(sum(1, -2)).toBe(-1);
+// });
+
+it.each`
+  a    | b     | expected
+  ${1} | ${2}  | ${3}
+  ${1} | ${-2} | ${-1}
+`("$aと$bを足すと$expectedになる", ({ a, b, expected }) => {
+  expect(sum(a, b)).toBe(expected);
 });
